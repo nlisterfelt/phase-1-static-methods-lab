39aa4120d60a8a98d1a181d731f8d05452f73238
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-23 19:09:36 -0500

    static methods

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+  static titleize(string){
+    string = this.capitalize(string)
+    const wordList = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const stringSplit = string.split(' ')
+    for(let i=0; i<stringSplit.length; i++){
+      if(wordList.includes(stringSplit[i])===false){
+        stringSplit[i] = this.capitalize(stringSplit[i])
+      }
+    }
+    return stringSplit.join(' ')
+  }
 }
+
+console.log(Formatter.titlesize('happy birthday and the new year'))
